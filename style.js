e828bd1c8e576edade778317361900848100f9d5
Kirubashankar K <48342585+KIRUBASHANKAR26@users.noreply.github.com>
2021-04-05 19:13:36 +0530

    Update style.js

--- style.js
+++ style.js
@@ -1,26 +1,25 @@
 
 function check()
 {
-  var emailInput=document.getElementById("email-input")
+  var emailInput=document.getElementById("email-input").value;
   var errorIcon=document.getElementById("error-img");
   var errorText=document.getElementById("text-error");
   var sucessText=document.getElementById("text-success");
-  if(!emailInput.checkValidity() || emailInput.value === "")
+  var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/
+  if(emailInput.match(pattern))
   { 
+    errorText.classList.remove("show");
+    sucessText.classList.add("show");
+    errorIcon.classList.remove("show");
+    emailInput.style.border = "1px solid hsl(0, 36%, 70%)";
+    emailInput.value=""; 
+  }else
+  {  
     errorText.classList.add("show");
     errorIcon.classList.add("show");
     sucessText.classList.remove("show");
     emailInput.style.border="3px solid hsl(0, 93%, 68%)";
     emailInput.value="";
-    
-  }else
-  {  
-    errorText.classList.remove("show");
-    sucessText.classList.add("show");
-    errorIcon.classList.remove("show");
-    emailInput.style.border = "1px solid hsl(0, 36%, 70%)";
-    emailInput.value="";
   }
 }
-
 
