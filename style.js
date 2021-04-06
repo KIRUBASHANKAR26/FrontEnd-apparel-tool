81d0dc2c5621481031c12ba1be81166b960e2427
Kirubashankar K <48342585+KIRUBASHANKAR26@users.noreply.github.com>
2021-04-06 13:35:31 +0530

    Update style.js

--- style.js
+++ style.js
@@ -5,24 +5,32 @@ mailForm.onclick = function(e) {
 function check()
 {
   var emailInput=document.getElementById("email-input").value;
+  var kk=document.getElementsByClassName("kk");
   var errorIcon=document.getElementById("error-img");
   var errorText=document.getElementById("text-error");
   var sucessText=document.getElementById("text-success");
   var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/
   if(emailInput.match(pattern))
   { 
+
     errorText.classList.remove("show");
     sucessText.classList.add("show");
     errorIcon.classList.remove("show");
-    emailInput.style.border = "1px solid hsl(0, 36%, 70%)";
-    emailInput.value=""; 
+    document.getElementById("email-input").value = "";
+    document.getElementById("email-input").style.border = "1px solid hsl(0, 36%, 70%)";
+    mailForm.value="";
+    
+    
   }else
   {  
+    
     errorText.classList.add("show");
     errorIcon.classList.add("show");
     sucessText.classList.remove("show");
-    emailInput.style.border="3px solid hsl(0, 93%, 68%)";
+    document.getElementById("email-input").style.border = "3px solid hsl(0, 93%, 68%)";
     emailInput.value="";
   }
 }
+
+
 
